764def8a4e1fd955c71b54ba82b4d3fa7bd7e2e4
ericsison <ericsison.dev@gmail.com>
2022-12-22 14:49:19 +0800

    fixed typo

--- apps/ims/src/pages/index.tsx
+++ apps/ims/src/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       <h1 className="text-5xl font-extrabold text-gray-700">
-        Inventory Management System
+        Inventory Management Systems
       </h1>
     </div>
   );
