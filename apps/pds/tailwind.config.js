d0ac7e543922a0dd5fb85c7fe60607621b535a45
leekimah <mikhailsebua@gmail.com>
2023-02-23 09:53:52 +0800

    changed tailwind config

--- apps/pds/tailwind.config.js
+++ apps/pds/tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
     // purge components from apps
     'apps/pds/src/components/**/*.{js,jsx,ts,tsx}',
 
+    // purge classes from apps
+    'apps/pds/src/classes/**/*.{js,ts,jsx,tsx}',
+
+    // purge context from apps
+    'apps/pds/src/context/**/*.{js,ts,jsx,tsx}',
+
     // purge shred-ui components from libs
     'libs/oneui/src/**/!(*.stories|*.spec).{ts,tsx,jsx,js,html}',
   ],
