5e8b782aed23aca4587e3d1ad49cbd8d80365ffb
toyaaponesto <jan.aponesto@gmail.com>
2023-02-23 16:27:47 +0800

    fixed broken path in local-storage

--- apps/portal/src/utils/hoc/applicants.tsx
+++ apps/portal/src/utils/hoc/applicants.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import useSWR from 'swr';
 import { fetchWithSession } from './fetcher';
 
@@ -8,11 +9,10 @@ export const getApplicants = async (vppId: string) => {
   const mockUrl = 'http://192.168.1.84:2000/auth/dashboard/';
 
   // get data from the backend
-  const { data: applicants } = useSWR(applicantGetUrl, fetchWithSession)
+  const { data: applicants } = useSWR(applicantGetUrl, fetchWithSession);
 
   // return employee data
   return applicants;
 };
-
 
 //  const applicantGetUrl = `${process.env.NEXT_PUBLIC_HRIS_URL}/applicant-endorsement/${vppId}/all`;
