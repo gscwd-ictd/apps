bcd4c0669d430d061598a605e1b454e50684360f
toyaaponesto <jan.aponesto@gmail.com>
2023-04-13 15:55:53 +0800

    updated pass slip store

--- apps/portal/src/types/leave.type.ts
+++ apps/portal/src/types/leave.type.ts
@@ -1,100 +1,100 @@
-export type LeaveDateRange = {
-  from: string;
-  to: string;
-};
+// export type LeaveDateRange = {
+//   from: string;
+//   to: string;
+// };
 
-//for filling out react hook form in leave application
-export type LeaveContents = {
-  employeeId: string;
-  dateOfFiling: string | null;
-  typeOfLeaveDetails: LeaveType;
+// //for filling out react hook form in leave application
+// export type LeaveContents = {
+//   employeeId: string;
+//   dateOfFiling: string | null;
+//   typeOfLeaveDetails: LeaveType;
 
-  leaveApplicationDates: Array<string>;
-  leaveApplicationDatesRange: LeaveDateRange;
+//   leaveApplicationDates: Array<string>;
+//   leaveApplicationDatesRange: LeaveDateRange;
 
-  inPhilippinesOrAbroad?: string; //withinThePhilippines or abroad
-  location?: string;
-  hospital?: string; //inHospital or outPatient
-  illness?: string | null;
-  specialLeaveWomenIllness?: string | null;
+//   inPhilippinesOrAbroad?: string; //withinThePhilippines or abroad
+//   location?: string;
+//   hospital?: string; //inHospital or outPatient
+//   illness?: string | null;
+//   specialLeaveWomenIllness?: string | null;
 
-  forMastersCompletion?: boolean;
-  forBarBoardReview?: boolean;
-  studyLeaveOther?: string | null; //applicable for Study Other only
+//   forMastersCompletion?: boolean;
+//   forBarBoardReview?: boolean;
+//   studyLeaveOther?: string | null; //applicable for Study Other only
 
-  other?: string | null; //monetization, terminal leave
+//   other?: string | null; //monetization, terminal leave
 
-  commutation?: string | null;
-  forMonetization?: boolean;
-  totalNumberOfDays: number; //number of days of leave
-};
+//   commutation?: string | null;
+//   forMonetization?: boolean;
+//   totalNumberOfDays: number; //number of days of leave
+// };
 
-export type LeaveId = Pick<LeaveContents, 'employeeId'>;
+// export type LeaveId = Pick<LeaveContents, 'employeeId'>;
 
-export type GetLeaveDetails = {
-  employeeDetails: {
-    assignment: {
-      id: string;
-      name: string;
-      positionId: string;
-      positionTitle: string;
-      salary: string;
-    };
-    companyId: string;
-    userId: string;
-    userRole: string;
-  };
+// export type GetLeaveDetails = {
+//   employeeDetails: {
+//     assignment: {
+//       id: string;
+//       name: string;
+//       positionId: string;
+//       positionTitle: string;
+//       salary: string;
+//     };
+//     companyId: string;
+//     userId: string;
+//     userRole: string;
+//   };
 
-  leaveApplicationBasicInfo: {
-    dateOfFiling: string;
-    id: string;
-    leaveDates: Array<string>;
-    leaveName: string;
-    status: string;
-  };
-  leaveApplicationDetails: {
-    inPhilippinesOrAbroad?: string;
-    location?: string;
-    hospital?: string;
-    illness?: string;
-    splWomen?: string;
-    forMastersCompletion?: string;
-    forBarBoardReview?: string;
-    studyLeaveOther?: string | null;
-  };
-};
+//   leaveApplicationBasicInfo: {
+//     dateOfFiling: string;
+//     id: string;
+//     leaveDates: Array<string>;
+//     leaveName: string;
+//     status: string;
+//   };
+//   leaveApplicationDetails: {
+//     inPhilippinesOrAbroad?: string;
+//     location?: string;
+//     hospital?: string;
+//     illness?: string;
+//     splWomen?: string;
+//     forMastersCompletion?: string;
+//     forBarBoardReview?: string;
+//     studyLeaveOther?: string | null;
+//   };
+// };
 
-//for fetching leave types from useSwr
-export type LeaveType = {
-  id: string;
-  leaveName: string;
-};
+// //for fetching leave types from useSwr
+// export type LeaveType = {
+//   id: string;
+//   leaveName: string;
+// };
 
-export type Leave = {
-  id: string;
-  leaveName: string;
-  dateOfFiling: string;
-  leaveDates: Array<string>;
-  status: string;
-};
+// export type Leave = {
+//   id: string;
+//   leaveName: string;
+//   dateOfFiling: string;
+//   leaveDates: Array<string>;
+//   status: string;
+// };
 
-export type LeaveList = {
-  completed: Array<Leave>;
-  ongoing: Array<Leave>;
-};
+// export type LeaveList = {
+//   completed: Array<Leave>;
+//   ongoing: Array<Leave>;
+// };
 
-export type ApprovalLeaveList = {
-  ongoing: Array<Leave>;
-  approved: Array<Leave>;
-  disapproved: Array<Leave>;
-};
+// export type ApprovalLeaveList = {
+//   ongoing: Array<Leave>;
+//   approved: Array<Leave>;
+//   disapproved: Array<Leave>;
+// };
 
-export type LeaveCredit = {
-  vacation: number;
-  sick: number;
-};
+// export type LeaveCredit = {
+//   vacation: number;
+//   sick: number;
+// };
 
-export type CalendarDate = {
-  date: string;
-  type: string;
-};
+// export type CalendarDate = {
+//   date: string;
+//   type: string;
+// };
--- apps/portal/src/types/passslip.type.ts
+++ apps/portal/src/types/passslip.type.ts
@@ -1,40 +1,40 @@
-export type PassSlip = {
-  employeeId: string;
-  dateOfApplication: string;
-  natureOfBusiness: string;
-  estimateHours?: number | null;
-  purposeDestination: string;
-  isCancelled: boolean;
-  obTransportation?: string | null;
-};
+// export type PassSlip = {
+//   employeeId: string;
+//   dateOfApplication: string;
+//   natureOfBusiness: string;
+//   estimateHours?: number | null;
+//   purposeDestination: string;
+//   isCancelled: boolean;
+//   obTransportation?: string | null;
+// };
 
-export type PassSlipList = {
-  ongoing: Array<PassSlipContents>;
-  completed: Array<PassSlipContents>;
-};
+// export type PassSlipList = {
+//   ongoing: Array<PassSlipContents>;
+//   completed: Array<PassSlipContents>;
+// };
 
-export type ApprovalPassSlipList = {
-  ongoing: Array<PassSlipContents>;
-  approved: Array<PassSlipContents>;
-  disapproved: Array<PassSlipContents>;
-};
+// export type ApprovalPassSlipList = {
+//   ongoing: Array<PassSlipContents>;
+//   approved: Array<PassSlipContents>;
+//   disapproved: Array<PassSlipContents>;
+// };
 
-export type PassSlipContents = {
-  createdAt: string;
-  updatedAt: string;
-  deletedAt: string | null;
-  id: string;
-  employeeId: string;
-  dateOfApplication: string;
-  natureOfBusiness: string;
-  obTransportation: string | null;
-  estimateHours: string;
-  purposeDestination: string;
-  isCancelled: boolean;
-  employeeName: string;
-  supervisorName: string;
-  supervisorId: string;
-  status: string;
-};
+// export type PassSlipContents = {
+//   createdAt: string;
+//   updatedAt: string;
+//   deletedAt: string | null;
+//   id: string;
+//   employeeId: string;
+//   dateOfApplication: string;
+//   natureOfBusiness: string;
+//   obTransportation: string | null;
+//   estimateHours: string;
+//   purposeDestination: string;
+//   isCancelled: boolean;
+//   employeeName: string;
+//   supervisorName: string;
+//   supervisorId: string;
+//   status: string;
+// };
 
-export type PassSlipId = Pick<PassSlipContents, 'id'>;
+// export type PassSlipId = Pick<PassSlipContents, 'id'>;
