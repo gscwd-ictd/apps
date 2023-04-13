d2def73b3817b14326a9728c4e2966d7523e1b60
allyncubero <allynjosephcubero@gmail.com>
2023-04-13 15:33:16 +0800

    Update to global pass slip types

--- libs/utils/src/lib/types/pass-slip.type.ts
+++ libs/utils/src/lib/types/pass-slip.type.ts
@@ -5,17 +5,34 @@ import {
   PassSlipStatus,
 } from '../../../../../apps/employee-monitoring/src/utils/enum/pass-slip.enum';
 
-export type PassSlip = {
-  id: string;
+// Pass slip application form
+export type PassSlipApplicationForm = {
   employeeId: string;
   dateOfApplication: string;
   natureOfBusiness: NatureOfBusiness | null;
   obTransportation: ObTransportation | null;
-  estimateHours: number | null;
+  estimateHours?: number | null;
   purposeDestination: string;
   isCancelled: boolean;
+};
+
+// List of pass slips per employee
+export type EmployeePassSlipList = {
+  completed: Array<PassSlipRow>;
+  ongoing: Array<PassSlipRow>;
+};
+
+// Single row type for individual or collated employees pass slip
+export type PassSlipRow = {
+  createdAt?: string;
+  updatedAt?: string;
+  deletedAt?: string | null;
+  id: string;
   employeeName: string;
   supervisorName: string;
   supervisorId: string;
   status: PassSlipStatus;
-};
+} & PassSlipApplicationForm;
+
+// Individual pass slip id
+export type PassSlipId = Pick<PassSlipRow, 'id'>;
