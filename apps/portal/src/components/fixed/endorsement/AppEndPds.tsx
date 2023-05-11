a0b4f066af4f0e0e5891134933d249faf161009c
leekimah <mikhailsebua@gmail.com>
2023-05-11 08:48:02 +0800

    Added pds viewing

--- apps/portal/src/components/fixed/endorsement/AppEndPds.tsx
+++ apps/portal/src/components/fixed/endorsement/AppEndPds.tsx
@@ -1,17 +1,19 @@
 /* eslint-disable @nx/enforce-module-boundaries */
 import { Applicant } from 'apps/portal/src/types/applicant.type';
 import fetcherHRIS from 'apps/portal/src/utils/helpers/fetchers/FetcherHRIS';
+import { isEmpty } from 'lodash';
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
+import { Pds } from '../../../../../pds/src/store/pds.store';
 
 type AppEndPdsProps = {
-  applicantDetails: Pick<Applicant, 'postingApplicantId' | 'applicantType'>;
+  applicantDetails: Pick<Applicant, 'applicantId' | 'applicantType'>;
 };
 
 export const AppEndPds = ({ applicantDetails }: AppEndPdsProps) => {
-  const externalApplicantUrl = `${process.env.NEXT_PUBLIC_HRIS_URL}/pds/${applicantDetails.postingApplicantId}`;
-  const internalApplicantUrl = `${process.env.NEXT_PUBLIC_PORTAL_URL}/api/pds/${applicantDetails.postingApplicantId}`;
-  const [pds, setPds] = useState<any>({});
+  const externalApplicantUrl = `${process.env.NEXT_PUBLIC_HRIS_URL}/pds/${applicantDetails.applicantId}`;
+  const internalApplicantUrl = `${process.env.NEXT_PUBLIC_PORTAL_URL}/api/pds/${applicantDetails.applicantId}`;
+  const [pds, setPds] = useState<Pds>({} as Pds);
 
   const {
     data: swrApplicant,
@@ -27,11 +29,14 @@ export const AppEndPds = ({ applicantDetails }: AppEndPdsProps) => {
   );
 
   useEffect(() => {
-    if (swrApplicant) {
-      console.log(pds);
-      setPds(swrApplicant);
+    if (swrIsLoading) setPds({} as Pds);
+  }, [swrIsLoading]);
+
+  useEffect(() => {
+    if (!isEmpty(swrApplicant)) {
+      setPds(swrApplicant.data);
     }
   }, [swrApplicant, swrError]);
 
-  return <>{swrIsLoading ? <></> : 'DONE'}</>;
+  return <>{swrIsLoading ? null : pds.personalInfo.firstName}</>;
 };
