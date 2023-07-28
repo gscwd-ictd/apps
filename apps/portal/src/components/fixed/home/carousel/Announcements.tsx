8b93f7f2b115d000bfdb649dc03b58fec70183d0
HRIS SVR <allynjosephcubero@gmail.com>
2023-07-28 15:38:30 +0800

    update dtr calendar, update carousel

---
+++ apps/portal/src/components/fixed/home/carousel/Announcements.tsx
@@ -0,0 +1,32 @@
+export const Announcements = [
+  {
+    title: 'GSCWD conducts DPPR 2023',
+    desc: 'Targets for CY 2024 were set during the Departmental Planning and Performance Review conducted by different departments of GSCWD. Scheduled for the whole month of June, each department revisited accomplishments, tackled concerns focusing on improving performance so to help the district in achieving its goals.(LGL)',
+    link: 'https://www.facebook.com/gensanwd/posts/pfbid0uRbNVg7KCCC2yutZjxt1x2ge2FD3XcYn9Q2Bc2EVYdccjhCE27m2eB9Xi1Tisp3Xl',
+    src: '/01.jpg',
+  },
+  {
+    title: 'GSCWD conducts DPPR 2023',
+    desc: 'Targets for CY 2024 were set during the Departmental Planning and Performance Review conducted by different departments of GSCWD. Scheduled for the whole month of June, each department revisited accomplishments, tackled concerns focusing on improving performance so to help the district in achieving its goals.(LGL)',
+    link: 'https://www.facebook.com/gensanwd/posts/pfbid0uRbNVg7KCCC2yutZjxt1x2ge2FD3XcYn9Q2Bc2EVYdccjhCE27m2eB9Xi1Tisp3Xl',
+    src: '/02.jpg',
+  },
+  {
+    title: 'GSCWD conducts DPPR 2023',
+    desc: 'Targets for CY 2024 were set during the Departmental Planning and Performance Review conducted by different departments of GSCWD. Scheduled for the whole month of June, each department revisited accomplishments, tackled concerns focusing on improving performance so to help the district in achieving its goals.(LGL)',
+    link: 'https://www.facebook.com/gensanwd/posts/pfbid0uRbNVg7KCCC2yutZjxt1x2ge2FD3XcYn9Q2Bc2EVYdccjhCE27m2eB9Xi1Tisp3Xl',
+    src: '/03.jpg',
+  },
+  {
+    title: 'Newly Hired Employees',
+    desc: 'Congratulations to our newly hired employees who joined our team on July 3, 2023.',
+    link: 'https://www.facebook.com/photo/?fbid=314144720967132&set=a.109004071481199',
+    src: '/04.jpg',
+  },
+  {
+    title: 'July Birthday Celebrants',
+    desc: 'Happy Birthday to all July Celebrants!',
+    link: 'https://www.facebook.com/gensanwd/videos/1959878211012530',
+    src: '/05.jpg',
+  },
+];
--- apps/portal/src/components/fixed/home/carousel/Carousel.tsx
+++ apps/portal/src/components/fixed/home/carousel/Carousel.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { useKeenSlider } from 'keen-slider/react';
 import 'keen-slider/keen-slider.min.css';
 import { useState } from 'react';
-import UseWindowDimensions from 'libs/utils/src/lib/functions/WindowDimensions';
+import { Announcements } from './Announcements';
 
 export default function Carousel() {
   const [next, setNext] = useState(false);
@@ -55,94 +55,29 @@ export default function Carousel() {
         ref={sliderRef}
         className="keen-slider w-full h-[150%] md:h-[280%] lg:h-screen pb-10 rounded-lg shadow bg-gray-100"
       >
-        <div className="keen-slider__slide number-slide1 w-screen h-screen">
-          <div className="w-screen h-full bg-gray-100 flex flex-col pt-4 pb-4 pl-8 pr-8 gap-2">
-            <label className="text-lg text-slate-600 uppercase">
-              {`GSCWD conducts DPPR 2023`}
-            </label>
-            <label className="hidden lg:block text-sm text-slate-500 text-justify">
-              {`Targets for CY 2024 were set during the Departmental Planning and Performance Review conducted by different departments of GSCWD.
-Scheduled for the whole month of June, each department revisited accomplishments, tackled concerns focusing on improving performance so to help the district in achieving its goals.(LGL)`}
-            </label>
+        {Announcements.map((src, i) => (
+          <>
+            <div className="keen-slider__slide number-slide1 w-screen h-screen">
+              <div className="w-screen h-full bg-gray-100 flex flex-col pt-4 pb-4 pl-8 pr-8 gap-2">
+                <label className="text-lg text-slate-600 uppercase">
+                  {src.title}
+                </label>
+                <label className="hidden lg:block text-sm text-slate-500 text-justify">
+                  {src.desc}
+                </label>
 
-            <label className="text-right text-sm text-slate-500 cursor-pointer">
-              <a
-                target="blank"
-                href="https://www.facebook.com/gensanwd/posts/pfbid0uRbNVg7KCCC2yutZjxt1x2ge2FD3XcYn9Q2Bc2EVYdccjhCE27m2eB9Xi1Tisp3Xl"
-              >
-                Read More
-              </a>
-            </label>
-            <img className="w-100 h-100" src="/01.jpg"></img>
-          </div>
-        </div>
-        <div className="keen-slider__slide number-slide2 w-screen h-screen">
-          <div className="w-screen h-full bg-gray-100 flex flex-col pt-4 pb-4 pl-8 pr-8 gap-2">
-            <label className="text-lg text-slate-600 uppercase">
-              {`GSCWD conducts DPPR 2023`}
-            </label>
+                <label className="text-right text-sm text-slate-500 cursor-pointer">
+                  <a target="blank" href={src.link}>
+                    Read More
+                  </a>
+                </label>
+                <img className="w-100 h-100" src={src.src}></img>
+              </div>
+            </div>
+          </>
+        ))}
 
-            <label className="hidden lg:block text-sm text-slate-500 text-justify">
-              {`Targets for CY 2024 were set during the Departmental Planning and Performance Review conducted by different departments of GSCWD.
-Scheduled for the whole month of June, each department revisited accomplishments, tackled concerns focusing on improving performance so to help the district in achieving its goals.(LGL)`}
-            </label>
-
-            <label className="text-right text-sm text-slate-500 cursor-pointer">
-              <a
-                target="blank"
-                href="https://www.facebook.com/gensanwd/posts/pfbid0uRbNVg7KCCC2yutZjxt1x2ge2FD3XcYn9Q2Bc2EVYdccjhCE27m2eB9Xi1Tisp3Xl"
-              >
-                Read More
-              </a>
-            </label>
-            <img className="w-100 h-100" src="/02.jpg"></img>
-          </div>
-        </div>
-        <div className="keen-slider__slide number-slide1 w-screen h-screen">
-          <div className="w-screen h-full bg-gray-100 flex flex-col pt-4 pb-4 pl-8 pr-8 gap-2">
-            <label className="text-lg text-slate-600 uppercase">
-              GSCWD conducts DPPR 2023
-            </label>
-
-            <label className="hidden lg:block text-sm text-slate-500 text-justify">
-              {`Targets for CY 2024 were set during the Departmental Planning and Performance Review conducted by different departments of GSCWD. Scheduled for the whole month of June, each department revisited accomplishments, tackled concerns focusing on improving performance so to help the district in achieving its goals.(LGL)`}
-            </label>
-
-            <label className="text-right text-sm text-slate-500 cursor-pointer">
-              <a
-                target="blank"
-                href="https://www.facebook.com/gensanwd/posts/pfbid0uRbNVg7KCCC2yutZjxt1x2ge2FD3XcYn9Q2Bc2EVYdccjhCE27m2eB9Xi1Tisp3Xl"
-              >
-                Read More
-              </a>
-            </label>
-            <img className="w-100 h-100" src="/03.jpg"></img>
-          </div>
-        </div>
-
-        <div className="keen-slider__slide number-slide1 w-screen h-screen">
-          <div className="w-screen h-full bg-gray-100 flex flex-col pt-4 pb-4 pl-8 pr-8 gap-2">
-            <label className="text-lg text-slate-600 uppercase">
-              Newly Hired Employees
-            </label>
-
-            <label className="hidden lg:block text-sm text-slate-500 text-justify">
-              {`Congratulations to our newly hired employees who joined our team on July 3, 2023.`}
-            </label>
-
-            <label className="text-right text-sm text-slate-500 cursor-pointer">
-              <a
-                target="blank"
-                href="https://www.facebook.com/photo/?fbid=314144720967132&set=a.109004071481199"
-              >
-                Read More
-              </a>
-            </label>
-            <img className="w-100 h-100" src="/04.jpg"></img>
-          </div>
-        </div>
-
-        <div className="keen-slider__slide number-slide1 w-screen h-screen">
+        {/* <div className="keen-slider__slide number-slide1 w-screen h-screen">
           <div className="w-screen h-full bg-gray-100 flex flex-col pt-4 pb-4 pl-8 pr-8 gap-2">
             <label className="text-lg text-slate-600 uppercase">
               July Birthday Celebrants
@@ -162,7 +97,7 @@ Scheduled for the whole month of June, each department revisited accomplishments
             </label>
             <img className="w-100 h-100" src="/05.jpg"></img>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
