3033cf3e97e8aee7c21b122b8b27bc365db8ed47
leekimah <mikhailsebua@gmail.com>
2023-08-14 08:23:51 +0800

    Replaced coming-soon page content

--- apps/employee-monitoring/src/pages/coming-soon.tsx
+++ apps/employee-monitoring/src/pages/coming-soon.tsx
@@ -7,12 +7,15 @@ export default function ComingSoon() {
         <div className="flex flex-col items-center justify-center h-full gap-1 ">
           {/** 404 */}
           <div className="flex items-center text-6xl font-bold text-blue-400">
-            <span className="-tracking-tighter">Coming Soon</span>
+            <span className="-tracking-tighter">Coming Soon!</span>
           </div>
 
           {/** Oops  */}
           <div className="flex items-center text-3xl font-semibold text-gray-400">
-            <span className="tracking-tighter"> This page is on-going!</span>
+            <span className="tracking-tighter">
+              {' '}
+              We are working on this feature
+            </span>
           </div>
 
           {/** Never existed  */}
