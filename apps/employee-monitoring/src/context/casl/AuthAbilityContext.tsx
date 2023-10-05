0ad7e1d9dfa591010853feacf1a85253835d289c
allyn <74882146+allyncubero@users.noreply.github.com>
2023-10-05 15:24:52 +0800

    Remove the mockdata sample

--- apps/employee-monitoring/src/context/casl/AuthAbilityContext.tsx
+++ apps/employee-monitoring/src/context/casl/AuthAbilityContext.tsx
@@ -1,20 +1,17 @@
 import { createContextualCan } from '@casl/react';
-import { createContext, ReactNode, useContext } from 'react';
-import { AuthContext } from '../AuthContext';
+import { createContext, ReactNode } from 'react';
 
 type AuthAbilityProviderProps = {
   children: ReactNode | ReactNode[];
 };
 
-// initialize create context and assign it to AuthAbilityContexxt
+// initialize create context and assign it to AuthAbilityContext
 export const AuthAbilityContext = createContext({});
 
 // can
 export const Can = createContextualCan(AuthAbilityContext.Consumer);
 
 export const AuthAbilityProvider = ({ children }: AuthAbilityProviderProps) => {
-  // const {} = useContext(AuthContext)
-
   return (
     <>
       <AuthAbilityContext.Provider value={{}}></AuthAbilityContext.Provider>
