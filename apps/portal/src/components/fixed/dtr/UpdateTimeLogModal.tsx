f2e6fd3e1e96a0262285f66131f004a2d5c22f73
gscwd-ictd <gensanwater.ictd@gmail.com>
2024-03-12 16:13:42 +0800

    update dtr time log modal UI

--- apps/portal/src/components/fixed/dtr/UpdateTimeLogModal.tsx
+++ apps/portal/src/components/fixed/dtr/UpdateTimeLogModal.tsx
@@ -134,6 +134,7 @@ const UpdateTimeLogModal: FunctionComponent<EditDailySchedModalProps> = ({
   useEffect(() => {
     if (modalState) {
       setDefaultValues(rowData, '');
+      console.log(rowData);
     }
   }, [modalState]);
 
@@ -210,8 +211,6 @@ const UpdateTimeLogModal: FunctionComponent<EditDailySchedModalProps> = ({
 
               <div className="flex flex-wrap justify-between">
                 <div className="flex flex-col justify-start items-start w-full sm:w-1/2 sm:pr-5 px-0.5 pb-3  ">
-                  {/* <label className="text-slate-500 text-md whitespace-nowrap pb-0.5">Time In:</label> */}
-
                   <div className="w-full ">
                     <LabelInput
                       textSize="sm"
@@ -242,14 +241,12 @@ const UpdateTimeLogModal: FunctionComponent<EditDailySchedModalProps> = ({
                             : 'bg-inherit'
                         }
                       `}
-                      disabled={rowData.dtr?.timeIn ? true : false}
+                      disabled={rowData.dtr?.timeIn ? true : rowData?.dtrCorrection?.status ? true : false}
                     />
                   </div>
                 </div>
 
                 <div className="flex flex-col justify-start items-start w-full sm:w-1/2 px-0.5 sm:pl-5 pb-3  ">
-                  {/* <label className="text-slate-500 text-md whitespace-nowrap pb-0.5 ">Lunch Out:</label> */}
-
                   <div className="w-full">
                     <LabelInput
                       textSize="sm"
@@ -279,14 +276,20 @@ const UpdateTimeLogModal: FunctionComponent<EditDailySchedModalProps> = ({
                             : 'bg-inherit'
                         }
                       `}
-                      disabled={getValues('withLunch') === true ? (rowData.dtr?.lunchOut ? true : false) : true}
+                      disabled={
+                        getValues('withLunch') === true
+                          ? rowData.dtr?.lunchOut
+                            ? true
+                            : rowData?.dtrCorrection?.status
+                            ? true
+                            : false
+                          : true
+                      }
                     />
                   </div>
                 </div>
 
                 <div className="flex flex-col justify-start items-start w-full sm:w-1/2 px-0.5 sm:pr-5 pb-3  ">
-                  {/* <label className="text-slate-500 text-md whitespace-nowrap pb-0.5 ">Lunch In:</label> */}
-
                   <div className="w-full">
                     <LabelInput
                       textSize="sm"
@@ -317,14 +320,20 @@ const UpdateTimeLogModal: FunctionComponent<EditDailySchedModalProps> = ({
                             : 'bg-inherit'
                         }
                       `}
-                      disabled={getValues('withLunch') === true ? (rowData.dtr?.lunchIn ? true : false) : true}
+                      disabled={
+                        getValues('withLunch') === true
+                          ? rowData.dtr?.lunchIn
+                            ? true
+                            : rowData?.dtrCorrection?.status
+                            ? true
+                            : false
+                          : true
+                      }
                     />
                   </div>
                 </div>
 
                 <div className="flex flex-col justify-start items-start w-full sm:w-1/2 px-0.5 sm:pl-5 pb-3  ">
-                  {/* <label className="text-slate-500 text-md whitespace-nowrap pb-0.5 ">Time Out:</label> */}
-
                   <div className="w-full">
                     <LabelInput
                       required
@@ -355,16 +364,18 @@ const UpdateTimeLogModal: FunctionComponent<EditDailySchedModalProps> = ({
                             : 'bg-inherit'
                         }
                       `}
-                      disabled={rowData?.dtr?.timeOut ? true : false}
+                      disabled={rowData?.dtr?.timeOut ? true : rowData?.dtrCorrection?.status ? true : false}
                     />
                   </div>
                 </div>
 
                 <div className="flex flex-col justify-start items-start w-full px-0.5 pb-3  ">
                   {rowData?.dtrCorrection?.remarks ? (
-                    <label className="font-medium text-gray-900 dark:text-gray-800 text-sm whitespace-nowrap pb-0.5 ">
-                      Remarks:
-                    </label>
+                    <>
+                      <label className="font-medium text-gray-900 dark:text-gray-800 text-sm whitespace-nowrap pb-0.5 ">
+                        Remarks:
+                      </label>
+                    </>
                   ) : null}
 
                   <div className="w-full">
@@ -409,6 +420,70 @@ const UpdateTimeLogModal: FunctionComponent<EditDailySchedModalProps> = ({
                     )}
                   </div>
                 </div>
+
+                {rowData?.dtrCorrection?.status ? (
+                  <>
+                    <div className="flex flex-col justify-start items-start w-full sm:w-1/2 sm:pr-5 px-0.5 pb-3  ">
+                      <div className="w-full ">
+                        <LabelInput
+                          textSize="sm"
+                          required
+                          id="updatedTimeIn"
+                          label="Updated Time In:"
+                          step="any"
+                          type="time"
+                          value={rowData.dtrCorrection.timeIn}
+                          disabled={true}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="flex flex-col justify-start items-start w-full sm:w-1/2 px-0.5 sm:pl-5 pb-3  ">
+                      <div className="w-full">
+                        <LabelInput
+                          textSize="sm"
+                          required
+                          id="updatedLunchOut"
+                          label="Updated Lunch Out:"
+                          step="any"
+                          type="time"
+                          value={rowData.dtrCorrection.lunchOut}
+                          disabled={true}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="flex flex-col justify-start items-start w-full sm:w-1/2 px-0.5 sm:pr-5 pb-3  ">
+                      <div className="w-full">
+                        <LabelInput
+                          textSize="sm"
+                          required
+                          id="updatedLunchIn"
+                          label="Updated Lunch In:"
+                          step="any"
+                          type="time"
+                          value={rowData.dtrCorrection.lunchIn}
+                          disabled={true}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="flex flex-col justify-start items-start w-full sm:w-1/2 px-0.5 sm:pl-5 pb-3  ">
+                      <div className="w-full">
+                        <LabelInput
+                          textSize="sm"
+                          required
+                          id="updatedTimeOut"
+                          label="Updated Time Out:"
+                          step="any"
+                          type="time"
+                          value={rowData.dtrCorrection.timeOut}
+                          disabled={true}
+                        />
+                      </div>
+                    </div>
+                  </>
+                ) : null}
               </div>
             </div>
           </form>
