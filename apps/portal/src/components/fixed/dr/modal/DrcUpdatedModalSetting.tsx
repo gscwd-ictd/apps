113154b8c57ec4fd6f5e5db204dd2b598dd68e35
leekimah <mikhailsebua@gmail.com>
2024-04-30 08:40:35 +0800

    Removed console log duty button

--- apps/portal/src/components/fixed/dr/modal/DrcUpdatedModalSetting.tsx
+++ apps/portal/src/components/fixed/dr/modal/DrcUpdatedModalSetting.tsx
@@ -363,9 +363,6 @@ export const DrcUpdatedModalSetting = () => {
           </section>
         </div>
       </div>
-      <button className="px-3 py-2 text-white bg-indigo-600 rounded" onClick={() => console.log(addedDrcs)}>
-        Log Duties
-      </button>
     </div>
   );
 };
