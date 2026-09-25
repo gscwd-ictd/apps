45fb2cf396a899d34df03182dd7d23e291ad9278
gscwd-ictd <gensanwater.ictd@gmail.com>
2026-09-25 12:07:29 +0800

    2026 pds udpate to job-portal

--- apps/job-portal/src/components/personal-data-sheet/WorkExperiencePdf.tsx
+++ apps/job-portal/src/components/personal-data-sheet/WorkExperiencePdf.tsx
@@ -130,34 +130,18 @@ export const WorkExperiencePdf = ({ formatDate, workExperience }: WorkExperience
         </View>
 
         {/* Monthly Salary */}
-        {/* <View
-          style={[
-            styles.borderRight,
-            styles.inputValue,
-            styles.horizontalCenter,
-            styles.w5_6,
-            { padding: 0 },
-          ]}
-        >
+        <View style={[styles.borderRight, styles.inputValue, styles.horizontalCenter, styles.w5_6, { padding: 0 }]}>
           <View style={[styles.verticalCenter]}>
             <Text>{experience.monthlySalary || 'N/A'}</Text>
           </View>
-        </View> */}
+        </View>
 
         {/* Salary Grade / Increment */}
-        {/* <View
-          style={[
-            styles.borderRight,
-            styles.inputValue,
-            styles.horizontalCenter,
-            styles.w5_6,
-            { padding: 1 },
-          ]}
-        >
+        <View style={[styles.borderRight, styles.inputValue, styles.horizontalCenter, styles.w5_6, { padding: 1 }]}>
           <View style={[styles.verticalCenter]}>
             <Text>{experience.salaryGrade || 'N/A'}</Text>
           </View>
-        </View> */}
+        </View>
 
         {/* Status of Appointment */}
         <View
@@ -236,18 +220,18 @@ export const WorkExperiencePdf = ({ formatDate, workExperience }: WorkExperience
           </View>
 
           {/* Monthly Salary */}
-          {/* <View style={[styles.borderRight, styles.inputValue, styles.horizontalCenter, styles.w5_6, { padding: 0 }]}>
+          <View style={[styles.borderRight, styles.inputValue, styles.horizontalCenter, styles.w5_6, { padding: 0 }]}>
             <View style={[styles.verticalCenter]}>
               <Text>N/A</Text>
             </View>
-          </View> */}
+          </View>
 
           {/* Salary Grade / Increment */}
-          {/* <View style={[styles.borderRight, styles.inputValue, styles.horizontalCenter, styles.w5_6, { padding: 1 }]}>
+          <View style={[styles.borderRight, styles.inputValue, styles.horizontalCenter, styles.w5_6, { padding: 1 }]}>
             <View style={[styles.verticalCenter]}>
               <Text>N/A</Text>
             </View>
-          </View> */}
+          </View>
 
           {/* Status of Appointment */}
           <View
@@ -329,37 +313,20 @@ export const WorkExperiencePdf = ({ formatDate, workExperience }: WorkExperience
         </View>
 
         {/* Monthly Salary */}
-        {/* <View
-          style={[
-            styles.borderRight,
-            styles.inputKey,
-            styles.horizontalCenter,
-            styles.w5_6,
-            { padding: 0 },
-          ]}
-        >
+        <View style={[styles.borderRight, styles.inputKey, styles.horizontalCenter, styles.w5_6, { padding: 0 }]}>
           <View style={[styles.verticalCenter]}>
             <Text style={{ fontSize: 5.7 }}>MONTHLY SALARY</Text>
           </View>
-        </View> */}
+        </View>
 
         {/* Salary Grade / Increment */}
-        {/* <View
-          style={[
-            styles.borderRight,
-            styles.inputKey,
-            styles.horizontalCenter,
-            styles.w5_6,
-            { padding: 1 },
-          ]}
-        >
+        <View style={[styles.borderRight, styles.inputKey, styles.horizontalCenter, styles.w5_6, { padding: 1 }]}>
           <View style={[styles.verticalCenter]}>
             <Text style={{ fontSize: 4.7 }}>
-              SALARY/ JOB/ PAY GRADE (if applicable)& STEP (Format
-              &quot;00-0&quot;)/ INCREMENT
+              SALARY/ JOB/ PAY GRADE (if applicable)& STEP (Format &quot;00-0&quot;)/ INCREMENT
             </Text>
           </View>
-        </View> */}
+        </View>
 
         {/* Status of Appointment */}
         <View style={[styles.borderRight, styles.inputKey, styles.horizontalCenter, styles.w9_5, { padding: 0 }]}>
